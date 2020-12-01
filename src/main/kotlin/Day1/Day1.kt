2843c1fce41dc1e0b2b86684ec63d163c0818fad
Josh Graham <Joshua.h.Graham96@gmail.com>
2020-12-01 16:40:18 -0500

    Initial Commit

---
+++ src/main/kotlin/Day1/Day1.kt
@@ -0,0 +1,45 @@
+import java.io.File
+
+fun main() {
+    var list = readFileLineByLineUsingForEachLine("test.txt")
+    list.sort();
+    processListTwoEntries(list)
+    processListThreeEntries(list)
+}
+
+fun processListThreeEntries(list: ArrayList<Int>) {
+    for (i in 0 until list.size) {
+        for (j in i + 1 until list.size) {
+            for (k in j + 1 until list.size) {
+                val a = list[i]
+                val b = list[j]
+                val c = list[k]
+                if (a + b + c == 2020) {
+                    println("a+b+c=$a+$b+$c where a*b*c is ${a * b * c}")
+                }
+            }
+        }
+    }
+}
+
+fun processListTwoEntries(list: ArrayList<Int>) {
+    for (i in 0 until list.size) {
+        for (j in i + 1 until list.size) {
+
+            val a = list[i]
+            val b = list[j]
+            if (a + b == 2020) {
+                println("a+b=$a+$b where a*b is ${a * b}")
+            }
+
+        }
+    }
+}
+
+fun readFileLineByLineUsingForEachLine(fileName: String): ArrayList<Int> {
+    var list = ArrayList<Int>()
+    File(fileName).forEachLine {
+        list.add(it.toInt())
+    }
+    return list;
+}
---
+++ src/main/kotlin/Day2/Day2.kt
@@ -0,0 +1,10 @@
+package Day2
+
+class Day2 {
+
+}
+
+fun main()
+{
+    println("Temp")
+}
