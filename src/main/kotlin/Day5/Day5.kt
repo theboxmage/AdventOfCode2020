8e7b1eff1d297e9d4b195cebb50d744194acf27c
Josh Graham <Joshua.h.Graham96@gmail.com>
2020-12-05 11:20:43 -0500

    Day 5, I learned a more elegant solution after I was done, and I'm kinda upset about it.

---
+++ src/main/kotlin/Day5/Day5.kt
@@ -0,0 +1,57 @@
+package Day5
+
+import java.io.File
+
+class Day5 {
+}
+
+fun main()
+{
+    val list = readFile("src\\main\\resources\\Day5\\input.txt")
+    val idList = ArrayList<Int>()
+    var maxId = -1
+    for( i in list)
+    {
+        val row = processString(i.take(7), 0, 127)
+        val column = processString(i.takeLast(3), 0, 7)
+        val id = row * 8 + column
+        idList.add(id)
+        println("ID = $id for row $row and column $column")
+        if(id > maxId)
+        {
+            maxId = id
+        }
+    }
+    idList.sort()
+    println("Max ID is: ${idList[idList.size-1]}")
+    for(x in 1 until idList.size-1)
+    {
+        if(idList[x] == idList[x+1]-2 || idList[x] == idList[x-1]-2)
+        {
+            println("My seat ID: ${idList[x+1]-1}")
+        }
+    }
+}
+
+fun processString(input: String, lowerBound: Int, upperBound: Int): Int {
+    var lowerCount = lowerBound
+    var upperCount = upperBound
+    for(char in input)
+    {
+        if(char == "B"[0] || char == "R"[0])
+        {
+            lowerCount = 1+((upperCount + lowerCount) / 2)
+        } else {
+            upperCount = (upperCount + lowerCount) / 2
+        }
+    }
+    return upperCount
+}
+
+fun readFile(fileName: String): ArrayList<String> {
+    val list = ArrayList<String>()
+    File(fileName).forEachLine {
+        list.add(it)
+    }
+    return list
+}
