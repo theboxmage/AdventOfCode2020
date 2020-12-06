bcd9dc1beba95f810f69dee8877cc866201edf5a
Josh Graham <Joshua.h.Graham96@gmail.com>
2020-12-06 15:26:02 -0500

    I can definitely clean this up some, but day 6 done

---
+++ src/main/kotlin/Day6/Day6.kt
@@ -0,0 +1,60 @@
+package Day6
+
+import java.io.File
+
+class Day6 {
+}
+
+fun main()
+{
+    val list = readFile("src\\main\\resources\\Day6\\input.txt")
+    var count = 0;
+    for(i in list)
+    {
+        count += i.size-1
+    }
+    println("Part 1: $count");
+    count = 0;
+    for(i in list)
+    {
+        println(i);
+
+        for(item in i.filter{(key, _) -> key != '#'})
+        {
+            if(item.value == i['#'])
+            {
+                count++;
+            }
+        }
+    }
+    println("Part 2: $count")
+}
+
+fun readFile(fileName: String): ArrayList<Map<Char, Int>> {
+    val list = ArrayList<Map<Char, Int>>()
+    var group = mutableMapOf<Char, Int>();
+    group['#'] = 0;
+    File(fileName).forEachLine {
+        if(it == "")
+        {
+            list.add(group);
+            group = mutableMapOf<Char, Int>();
+            group['#'] = 0;
+        }
+        else {
+            val inputs = it.split("");
+            group['#'] = group['#']!!.plus(1);
+            for(character in it)
+            {
+                if(group[character] == null)
+                {
+                    group[character] = 1;
+                } else {
+                    group[character] = group[character]!! + 1;
+                }
+            }
+        }
+    }
+    list.add(group);
+    return list
+}
